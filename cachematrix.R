ee0c79cabd3197359d1dd8d25a723f60612f3bf0
Ussia <46683095+Ussia001@users.noreply.github.com>
2019-03-09 13:22:19 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,65 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## The code is able to create an matrix, and cache its inverse until it can be called upon later
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  ## The first part of the code will set the inverse
+  i <- NULL
+  
+  ## Subsection to initiate the matrix
+    set <- function( matrix ) {
+      m <<- matrix
+      i <<- NULL
+  }
+  
+  ## Here the matrix will be called upon from the cache memory
+  get <- function() {
+    
+    ## The matrix will be displayed 
+    m
+  }
+  
+  ## The inverse of the matrix will be set here
+  setInverse <- function(inverse) {
+    i <<- inverse
+  }
+  
+  ##The inverse of the matrix will be retrieved from cache memory
+  getInverse <- function() {
+    
+    ## The Inverse of the matrix will be displayed here
+    i
+  }
+  ## The functions/ methods used will be displayed here
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
 
-}
-
-
-## Write a short comment describing this function
+## The inverse of the special matrix is created from the "makeCacheMatrix" section above
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+        ## The inverse of 'x' is displayed as a Matrix
+  m <- x$getInverse()
+  
+  ## The inverse will be shown/diplayedif its already set
+  if( !is.null(m) ) {
+    
+           message("getting cached data")
+            return(m)
+  }
+  
+  ## Matrix is retrieved
+  data <- x$get()
+  
+  ## matrix multiplication used to compute matrix
+  m <- solve(data) %*% data
+  
+  ## The inverse is set to the object m
+  x$setInverse(m)
+  
+  ## The matrix is displayed by calling on the object
+  m
 }
